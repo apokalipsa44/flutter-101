9711775861b534e8ec9271980d8bf53d4b37a7e7
apokalipsa44 <m_lachowicz@wp.pl>
2020-06-09 23:22:12 +0200

    piano done

--- cymbalki/lib/main.dart
+++ cymbalki/lib/main.dart
@@ -5,20 +5,39 @@ void main() {
   runApp(Cymbalki());
 }
 
+void playNote(int noteNumber) {
+  final audioPlayer = AudioCache();
+  audioPlayer.play('note$noteNumber.wav');
+}
+
+Expanded pianoKey(int noteNumber, Color keyColor) {
+  return Expanded(
+    child: FlatButton(
+      color: keyColor,
+      onPressed: () {
+        playNote(noteNumber);
+      },
+    ),
+  );
+}
+
 class Cymbalki extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(
-            child: FlatButton(
-              onPressed: () {
-                final audioPlayer = AudioCache();
-                audioPlayer.play('note1.wav');
-              },
-              child: Text('play'),
-            ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              pianoKey(1, Colors.red),
+              pianoKey(2, Colors.greenAccent),
+              pianoKey(3, Colors.pink),
+              pianoKey(4, Colors.blue),
+              pianoKey(5, Colors.yellow),
+              pianoKey(6, Colors.purple),
+              pianoKey(7, Colors.grey),
+            ],
           ),
         ),
       ),
