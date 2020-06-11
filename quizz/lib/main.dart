fbd9c2fee914258e37c5e4029b9a97a5bcb0dfd6
apokalipsa44 <m_lachowicz@wp.pl>
2020-06-11 17:59:12 +0200

    layout done

--- quizz/lib/main.dart
+++ quizz/lib/main.dart
@@ -30,16 +30,56 @@ class _QuizPageState extends State<QuizPage> {
   @override
   Widget build(BuildContext context) {
     return Column(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: <Widget>[
-        Expanded(
-          flex: 5,
-          child: Padding(padding: EdgeInsets.all(10.0),
-            child: Center(
-              child: Text("ssij", textAlign: TextAlign.center,),
-            ),),),
-      ],
-    )
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Expanded(
+            flex: 5,
+            child: Padding(
+              padding: EdgeInsets.all(10.0),
+              child: Center(
+                child: Text(
+                  "ssij",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: EdgeInsets.all(16.0),
+              child: FlatButton(
+                onPressed: () {
+                  answerCheck();
+                },
+                color: Colors.green,
+                child: Text(
+                  'True',
+                  style: TextStyle(color: Colors.white, fontSize: 20.0),
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: EdgeInsets.all(16.0),
+              child: FlatButton(
+                onPressed: () {
+                  answerCheck();
+                },
+                color: Colors.red,
+                child: Text(
+                  'False',
+                  style: TextStyle(color: Colors.white, fontSize: 20.0),
+                ),
+              ),
+            ),
+          )
+        ]);
+  }
+
+  answerCheck() {
+    print('answer');
   }
 }
