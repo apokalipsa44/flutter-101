3802a5d38071e894de04b129dfef18cd4d034ea4
apokalipsa44 <m_lachowicz@wp.pl>
2020-08-12 11:30:32 +0200

    task screen layout done

--- todo_list/lib/screens/task_screen.dart
+++ todo_list/lib/screens/task_screen.dart
@@ -1,8 +1,64 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class TaskScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.lime,
+      body: Container(
+        padding: EdgeInsets.only(left: 12, top: 60, right: 12, bottom: 12),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: CircleAvatar(
+                radius: 40,
+                backgroundColor: Colors.white,
+                child: Icon(
+                  Icons.list,
+                  color: Colors.lime,
+                  size: 60,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 8, top: 15),
+              child: Text(
+                'todo_list_app',
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 30,
+                    color: Colors.lime,
+                    backgroundColor: Colors.white),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                '12 todos',
+                style: TextStyle(fontSize: 18, color: Colors.white),
+              ),
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.only(top: 20, bottom: 0),
+                child: Container(
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(10),
+                        topRight: Radius.circular(10),
+                        bottomLeft: Radius.circular(2),
+                        bottomRight: Radius.circular(2),
+                      )),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
